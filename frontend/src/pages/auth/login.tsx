2689739e4dcbcf5bd5775c755feeee80d5f253b2
Camilla Cino <camillacino96@gmail.com>
2025-06-20 12:32:06 +0200

    modifiche login

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -19,7 +19,7 @@ export default function Login() {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const res = await fetch('http://localhost:4000/api/login', {
+    const res = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ nomeUtente, password }),
